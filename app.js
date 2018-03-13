7910922d01b32941dcc97c7bac973f51a7c504c5
ming3000 <xuming.js@outlook.com>
2018-03-13 13:13:56 +0800

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const config = require('./config/config');
+
+const mongodb = require('./models/mongodb');
+const db = mongodb();
+
+const kafka = require('kafka-node');
+const Consumer = kafka.Consumer;
+const Offset = kafka.Offset;
+const Client = kafka.Client;
+
+const topic = config.kafka_topic;
+
+const client = new Client(config.kafka_host);
+const topics = [{topic: topic, partition: 0}];
+const options = {autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024};
+
+const consumer = new Consumer(client, topics, options);
+const offset = new Offset(client);
+
+consumer.on('message', function (message) {
+    console.log('kafka consumer: ' + message);
+});
+
+consumer.on('error', function (err) {
+    console.log('kafka error: ' + err);
+});
--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const config = {
     // db_uri: "mongodb://username:password@host:port/database"
-    db_uri: "mongodb://username:password@host:port/database",
+    db_uri: "mongodb://localhost/test_database",
 
-    kafka_host: "",
-    kafka_topic: "",
+    kafka_host: "localhost:2181",
+    kafka_topic: "test",
 };
 
 module.exports = config;
